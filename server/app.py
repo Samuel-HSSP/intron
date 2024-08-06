e03efd492e33d9b2bf6c14f485823bc13dcd55f5
Lyxodius <lyxodius@gmail.com>
2024-08-06 14:52:47 +0100

    Intron interview project

---
+++ server/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(80), nullable=False)
+    lastName = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+with app.app_context():
+    # User.__table__.drop(db.engine)
+    db.create_all()
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+    new_user = User(
+        firstName=data['firstName'],
+        lastName=data['lastName'],
+        username=data['username'],
+        email=data['email'],
+        password=data['password']
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User created successfully', 'user': {'id': user.id}}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    user = User.query.filter_by(email=data['email'], password=data['password']).first()
+    if user:
+        return jsonify({'message': 'Login successful', 'user': {'id': user.id, 'username': user.username, 'email': user.email}})
+    else:
+        return jsonify({'message': 'Invalid credentials'}), 401
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users_list = [{
+        'firstName': user.firstName,
+        'lastName': user.lastName,
+        'username': user.username,
+        'email': user.email
+    } for user in users]
+    return jsonify(users_list)
+
+@app.route('/user/<int:id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return jsonify({
+        'firstName': user.firstName,
+        'lastName': user.lastName,
+        'username': user.username,
+        'email': user.email
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
